8fb533fdeaf393bca2b62bd1c6403faaf36bfd7f
dduniverse <suhyunbory@gmail.com>
2024-11-21 17:08:28 +0900

    feat: QueryClient 정의

--- src/services/pokemon/query.ts
+++ src/services/pokemon/query.ts
@@ -6,13 +6,11 @@ export const pokemonService = {
     queryOptions({
       queryKey: ['pokemonData', region, offset],
       queryFn: () => fetchPokemonData(region, offset),
-      staleTime: 1000 * 60 * 5, 
     }),
 
   getPokemonDetailData: (pokemonId: number) =>
     queryOptions({
       queryKey: ['pokemonDetail', pokemonId],
       queryFn: () => fetchPokemonDetailData(pokemonId),
-      staleTime: 1000 * 60 * 5, 
     }),
 }
