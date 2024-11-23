e0f244ac48bfaf1de4b3b1f057f60caffe68356b
dduniverse <suhyunbory@gmail.com>
2024-11-23 15:42:48 +0900

    feat: detail prefetch

--- src/components/atom/evolution-box.tsx
+++ src/components/atom/evolution-box.tsx
@@ -1,16 +1,25 @@
 import Link from "next/link";
 import { GroupedEvolution } from "@/types/types";
 import { PokemonImage } from "@/components/atom";
+import { useDetailPrefetch } from "@/hooks/use-detail-prefetch";
 
 interface EvolutionBoxProps {
   pokemon: GroupedEvolution;
 }
 export default function EvolutionBox({ pokemon }: EvolutionBoxProps) {
+
+  const prefetchPokemonData = useDetailPrefetch();
+
+  const handleHover = () => {
+    prefetchPokemonData(pokemon.id);
+  }
+
   return (
     <Link href={`/${pokemon.id}`}>
       <div
         className="flex justify-center items-center w-72 rounded-lg p-4 gap-1 transition-transform duration-300 ease-in-out
                   transform hover:scale-105 shadow-sm hover:shadow-lg bg-gray-100 cursor-pointer overflow-hidden"
+        onMouseEnter={handleHover}
       >
         <PokemonImage id={pokemon.id} name={pokemon.name} />
         <p className="font-bold text-lg">{pokemon.name}</p>
--- src/components/molecule/nav-button.tsx
+++ src/components/molecule/nav-button.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { PokemonImage } from "@/components/atom";
+import { useDetailPrefetch } from "@/hooks/use-detail-prefetch";
 
 interface NavigationButtonProps {
   disabled: boolean; 
@@ -8,6 +9,13 @@ interface NavigationButtonProps {
 }
 
 export default function NavButton({ disabled, id, name }: NavigationButtonProps) {
+
+  const prefetchPokemonData = useDetailPrefetch();
+
+  const handleHover = () => {
+    prefetchPokemonData(id);
+  }
+
   return (
     <Link href={`/${id}`}>
       <button
@@ -15,6 +23,7 @@ export default function NavButton({ disabled, id, name }: NavigationButtonProps)
         className={`flex items-center justify-center gap-1 px-3 py-1 font-bold text-white rounded-md transition-colors
           ${disabled ? "bg-gray-300 cursor-not-allowed" : "bg-yellow-500 hover:bg-yellow-600"}
         `}
+        onMouseEnter={handleHover}
       >
         <PokemonImage id={id} name={name} width={40} height={40} />
         {name ? `No. ${id}` : "No Pokemon"}
