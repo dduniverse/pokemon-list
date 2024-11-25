46315cc18246dbc103e1a743e7e84ebba355c48c
김수현 (NIT기술팀) <suhyun.kimm@kt.com>
2024-11-25 16:01:23 +0900

    fix: query key

--- src/services/pokemon/query.ts
+++ src/services/pokemon/query.ts
@@ -2,9 +2,9 @@ import { queryOptions } from "@tanstack/react-query";
 import { fetchPokemonData, fetchPokemonDetailData } from "./fetch";
 
 export const pokemonService = {
-  getPokemonData: (region, offset) =>
+  getPokemonData: (region: string, offset: number) =>
     queryOptions({
-      queryKey: ['pokemonData', region, offset],
+      queryKey: region === "All" ? ['pokemonData', region, offset]: ['pokemonData', region],
       queryFn: () => fetchPokemonData(region, offset),
     }),
 
