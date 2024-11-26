efcfbb5312717af7db89bfc44793de8e013c305b
김수현 (NIT기술팀) <suhyun.kimm@kt.com>
2024-11-26 13:55:16 +0900

    refactor: query-error 발생 시 토스트 메세지

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { csrClient } from "@/services/react-query";
 import "../styles/globals.scss";
+import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>Pokemon</title>
       </Head>
+      <Toaster position="top-right" reverseOrder={false} />
       <Component {...pageProps} />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
